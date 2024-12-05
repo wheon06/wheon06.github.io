b6ee83fdf3148929f0cb4b6e5d0c09ece3e5a6a7
Heeyeon Lee <up2kid@gmail.com>
2024-12-05 15:27:45 +0900

    remove: delete format-date.ts

--- src/app/util/format-date.ts
+++
@@ -1,6 +0,0 @@
-export const formatDate = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1);
-  const day = String(date.getDate());
-  return `${year}년 ${month}월 ${day}일`;
-};
