2fc1fb2809564c2981381d9185228a0e670d2787
Heeyeon Lee <up2kid@gmail.com>
2024-12-05 16:39:05 +0900

    feat: add custom not found page

---
+++ src/app/404.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className='flex min-h-screen w-full items-center justify-center text-xl font-semibold'>
+      404 | Not Found
+    </div>
+  );
+};
+
+export default NotFound;
