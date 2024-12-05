d7139469c3522a2c1e1cbfb07afab13ce3c9c0eb
Heeyeon Lee <up2kid@gmail.com>
2024-12-05 15:27:38 +0900

    remove: delete fetcher.ts

--- src/app/util/fetcher.ts
+++
@@ -1,9 +0,0 @@
-import { MethodType } from '@/app/types/method-type';
-
-export const fetcher = async (uri: string, method: keyof typeof MethodType) => {
-  const response = await fetch('http://localhost:4000' + uri, {
-    method: MethodType[method],
-  });
-
-  return response;
-};
