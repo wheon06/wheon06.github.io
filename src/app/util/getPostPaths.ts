5b51b0de2a4cbd201c2a520c8e6de1f9f10d4c58
Heeyeon Lee <up2kid@gmail.com>
2024-12-09 16:33:08 +0900

    feat: add category filter

--- src/app/util/getPostPaths.ts
+++ src/app/util/getPostPaths.ts
@@ -5,6 +5,7 @@ const BASE_PATH = '/src/app/posts';
 const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
 
 export const getPostPaths = (category?: string) => {
-  const folder = category || '**';
-  return sync(`${POSTS_PATH}/${folder}/**/*.mdx`);
+  if (category)
+    return sync(`${POSTS_PATH}/${decodeURIComponent(category)}/**/*.mdx`);
+  else return sync(`${POSTS_PATH}/**/**/*.mdx`);
 };
