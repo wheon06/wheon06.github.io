d671d61a94bb9b83cc7ab82700efc50519d26a09
Heeyeon Lee <up2kid@gmail.com>
2024-12-12 14:18:57 +0900

    Updates

---
+++ _next/static/chunks/pages/_app-6c13384539fdfd4a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{6865:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(3154)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(6865),_(7734)}),_N_E=n.O()}]);
