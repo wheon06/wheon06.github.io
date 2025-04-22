192a55836411b12dea200e51295f2ac3a6a43647
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-04-22 13:34:10 +0900

    feature: copyright

--- src/app/components/post-footer.tsx
+++ src/app/components/post-footer.tsx
@@ -1,7 +1,9 @@
 export const PostFooter = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className='flex w-full justify-center gap-1 bg-white py-10 text-black'>
-      <p>© 2024. </p>
+      <p>© 2024-{currentYear}. </p>
       <p className='font-semibold'>Heeyeon Lee</p>
       <p>all rights reserved.</p>
     </div>
