20957576616162ac919eb01d208d881b596536f3
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-04-29 14:47:27 +0900

    feature: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -35,20 +35,19 @@ const formatToOriginDate = (koreanDate: string) => {
 const generateSitemapXml = (
   urls: { loc: string; lastmod: Date | string | null }[],
 ) => {
-  return `
-    <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${urls
-          .map(({ loc, lastmod }) => {
-            const lastmodString = lastmod
-              ? new Date(lastmod).toISOString()
-              : new Date().toISOString();
-            return `
-          <url>
-            <loc>${loc}</loc>
-            <lastmod>${lastmodString}</lastmod>
-          </url>`;
-          })
-          .join('')}
-  </urlset>`;
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls
+    .map(({ loc, lastmod }) => {
+      const lastmodString = lastmod
+        ? new Date(lastmod).toISOString()
+        : new Date().toISOString();
+      return `
+    <url>
+      <loc>${loc}</loc>
+      <lastmod>${lastmodString}</lastmod>
+    </url>`;
+    })
+    .join('')}
+</urlset>`;
 };
